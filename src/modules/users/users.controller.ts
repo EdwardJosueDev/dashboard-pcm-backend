674ff1a27dc2b3b229e97f6f6df10d503c51b5e9
Edward Josue Mamani M. <edward.mamani@bartech.pe>
2025-12-17 10:27:45 -0500

    fix

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -14,16 +14,19 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { PaginatedResponse } from '@modules/common/interfaces/paginated-response.interface';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiBearerAuth()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiBearerAuth()
   @Get()
   findAll(): Promise<PaginatedResponse<User>> {
     return this.usersService.findAll();
@@ -34,6 +37,7 @@ export class UsersController {
     return this.usersService.findOne(id);
   }
 
+  @ApiBearerAuth()
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -42,6 +46,7 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @ApiBearerAuth()
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
